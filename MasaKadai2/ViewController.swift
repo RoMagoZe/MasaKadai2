996cacfeb460f4db25a172974c7c45fd80665c2c
akio0911 <akio0911@gmail.com>
2022-10-11 19:30:21 +0900

    feedback

--- MasaKadai2/ViewController.swift
+++ MasaKadai2/ViewController.swift
@@ -13,22 +13,26 @@ class ViewController: UIViewController {
     @IBOutlet weak private var numberInputTextField2: UITextField!
     @IBOutlet weak private var calcStatus: UISegmentedControl!
     @IBOutlet weak private var showNumberLabel: UILabel!
-    @IBAction func resultButton(_ sender: Any) {
 
+    @IBAction func resultButton(_ sender: Any) {
         let numberInput1 = Float(numberInputTextField1.text ?? "") ?? 0
         let numberInput2 = Float(numberInputTextField2.text ?? "") ?? 0
 
-        if calcStatus.selectedSegmentIndex == 0 {
+        switch calcStatus.selectedSegmentIndex {
+        case 0:
             showNumberLabel.text = String(numberInput1 + numberInput2)
-        } else if calcStatus.selectedSegmentIndex == 1 {
+        case 1:
             showNumberLabel.text = String(numberInput1 - numberInput2)
-        } else if calcStatus.selectedSegmentIndex == 2 {
+        case 2:
             showNumberLabel.text = String(numberInput1 * numberInput2)
-        } else if calcStatus.selectedSegmentIndex == 3 {
-            showNumberLabel.text = String(numberInput1 / numberInput2)
-            if numberInput1 == 0 || numberInput2 == 0 {
+        case 3:
+            if numberInput2 == 0 {
                 showNumberLabel.text = "割る数には0以外を入力して下さい！"
+            } else {
+                showNumberLabel.text = String(numberInput1 / numberInput2)
             }
+        default:
+            assertionFailure("selectedSegmentIndex is invalid.")
         }
     }
 }
